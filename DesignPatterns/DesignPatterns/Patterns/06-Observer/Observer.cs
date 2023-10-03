343ac87a3528a477b583a1964137006e4508c874
Jeff <jeff@renderheads.com>
2023-10-03 20:00:37 +0200

    adds observer

---
+++ DesignPatterns/DesignPatterns/Patterns/06-Observer/Observer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace DesignPatterns.Patterns.Observer
+{
+    public interface IObserver
+    {
+        void Update(string message);
+    }
+
+    public interface ISubject
+    {
+        void RegisterObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers();
+    }
+
+    public class ConcreteSubject : ISubject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+        private string state;
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(state);
+            }
+        }
+
+        public void SetState(string newState)
+        {
+            state = newState;
+            NotifyObservers();
+        }
+    }
+
+    public class ConcreteObserver : IObserver
+    {
+        private string name;
+
+        public ConcreteObserver(string name)
+        {
+            this.name = name;
+        }
+
+        public void Update(string message)
+        {
+            Console.WriteLine($"{name} received message: {message}");
+        }
+    }
+
+}
+
---
+++ DesignPatterns/DesignPatternsTests/06-Observer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using DesignPatterns;
+using DesignPatterns.Patterns.MVC;
+using DesignPatterns.Patterns.Observer;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+
+namespace DesignPatternsTests
+{
+    public class Observer
+    {
+        private StringWriter consoleOutput;
+        private TextWriter originalConsoleOut;
+
+        [SetUp]
+        public void RedirectConsoleOutput()
+        {
+            // Redirect console output to a StringWriter
+            consoleOutput = new StringWriter();
+            originalConsoleOut = Console.Out;
+            Console.SetOut(consoleOutput);
+        }
+
+        [TearDown]
+        public void RestoreConsoleOutput()
+        {
+            // Restore the original console output and dispose of StringWriter
+            Console.SetOut(originalConsoleOut);
+            consoleOutput.Dispose();
+        }
+
+        [Test]
+        public void ConcreteObserver_UpdatesWhenNotified()
+        {
+            // Arrange
+            var concreteSubject = new ConcreteSubject();
+            var observerA = new ConcreteObserver("Observer A");
+            var observerB = new ConcreteObserver("Observer B");
+
+            // Act
+            concreteSubject.RegisterObserver(observerA);
+            concreteSubject.RegisterObserver(observerB);
+            concreteSubject.SetState("New State");
+
+            // Assert
+            string consoleText = consoleOutput.ToString();
+            StringAssert.Contains("Observer A received message: New State", consoleText);
+            StringAssert.Contains("Observer B received message: New State", consoleText);
+        }
+
+        [Test]
+        public void ConcreteObserver_DoesNotUpdateAfterRemoval()
+        {
+            // Arrange
+            var concreteSubject = new ConcreteSubject();
+            var observerA = new ConcreteObserver("Observer A");
+            var observerB = new ConcreteObserver("Observer B");
+
+            // Act
+            concreteSubject.RegisterObserver(observerA);
+            concreteSubject.RegisterObserver(observerB);
+            concreteSubject.RemoveObserver(observerA);
+            concreteSubject.SetState("New State");
+
+            // Assert
+            string consoleText = consoleOutput.ToString();
+            StringAssert.DoesNotContain("Observer A received message:", consoleText);
+            StringAssert.Contains("Observer B received message: New State", consoleText);
+        }
+    }
+}
+
