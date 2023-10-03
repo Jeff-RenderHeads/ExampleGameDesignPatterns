ed5c6929d789d0ee5ee3496f183e1ca54e9a6cf4
Jeff <jeff@renderheads.com>
2023-10-03 19:49:47 +0200

    adds MVC

---
+++ DesignPatterns/DesignPatterns/Patterns/05-MVC/MVC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace DesignPatterns.Patterns.MVC
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class UserView
+    {
+        public void DisplayUser(UserModel user)
+        {
+            Console.WriteLine($"Name: {user.Name}");
+            Console.WriteLine($"Age: {user.Age}");
+        }
+    }
+
+    public class UserController
+    {
+        private UserModel model;
+        private UserView view;
+
+        public UserController(UserModel model, UserView view)
+        {
+            this.model = model;
+            this.view = view;
+        }
+
+        public void UpdateUserData(string name, int age)
+        {
+            model.Name = name;
+            model.Age = age;
+        }
+
+        public void DisplayUserData()
+        {
+            view.DisplayUser(model);
+        }
+    }
+
+}
+
---
+++ DesignPatterns/DesignPatternsTests/05-MVCtests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using DesignPatterns;
+using DesignPatterns.Patterns.MVC;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+
+namespace DesignPatternsTests
+{
+    public class MVCTests
+    {
+        private StringWriter consoleOutput;
+        private TextWriter originalConsoleOut;
+
+        [SetUp]
+        public void RedirectConsoleOutput()
+        {
+            // Redirect console output to a StringWriter
+            consoleOutput = new StringWriter();
+            originalConsoleOut = Console.Out;
+            Console.SetOut(consoleOutput);
+        }
+
+        [TearDown]
+        public void RestoreConsoleOutput()
+        {
+            // Restore the original console output
+            Console.SetOut(originalConsoleOut);
+            consoleOutput.Dispose();
+        }
+
+        [Test]
+        public void UserController_UpdateUserData_ShouldUpdateModel()
+        {
+            // Arrange
+            var userModel = new UserModel();
+            var userView = new UserView();
+            var userController = new UserController(userModel, userView);
+
+            // Act
+            userController.UpdateUserData("John Doe", 30);
+
+            // Assert
+            Assert.That(userModel.Name == "John Doe");
+            Assert.That(userModel.Age == 30);
+        }
+
+        [Test]
+        public void UserController_DisplayUserData_ShouldDisplayInView()
+        {
+            // Arrange
+            var userModel = new UserModel { Name = "Alice", Age = 25 };
+            var userView = new UserView();
+            var userController = new UserController(userModel, userView);
+
+            // Act
+            userController.DisplayUserData();
+
+            // Assert
+            string consoleText = consoleOutput.ToString();
+            StringAssert.Contains("Name: Alice", consoleText);
+            StringAssert.Contains("Age: 25", consoleText);
+        }
+    }
+}
+
