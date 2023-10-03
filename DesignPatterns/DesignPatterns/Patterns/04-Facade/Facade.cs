9fb1c7ae70162e6e75a2f76759101e5eba813e4c
Jeff <jeff@renderheads.com>
2023-10-03 17:37:42 +0200

    adds facade

---
+++ DesignPatterns/DesignPatterns/Patterns/04-Facade/Facade.cs
@@ -0,0 +1,115 @@
+﻿using System;
+namespace DesignPatterns
+{
+    // Subsystem: DVD Player
+    public class DVDPlayer
+    {
+        public bool IsOn = false;
+        public string MovieName;
+
+        public void TurnOn()
+        {
+            Console.WriteLine("DVD Player is ON.");
+            IsOn = true;
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("DVD Player is OFF.");
+            IsOn = false;
+            MovieName = "";
+        }
+
+        public void PlayMovie(string movie)
+        {
+            Console.WriteLine($"Playing the movie: {movie}");
+            MovieName = movie;
+        }
+    }
+
+    // Subsystem: Projector
+    public class Projector
+    {
+        public bool IsOn = false;
+        public string InputName;
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Projector is ON.");
+            IsOn = true;
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Projector is OFF.");
+            IsOn = false;
+            InputName = "";
+        }
+
+        public void SetInput(string input)
+        {
+            Console.WriteLine($"Setting input to: {input}");
+            InputName = input;
+        }
+    }
+
+    // Subsystem: Audio System
+    public class AudioSystem
+    {
+        public bool IsOn = false;
+        public int Volume;
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Audio System is ON.");
+            IsOn = true;
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Audio System is OFF.");
+            IsOn = false;
+        }
+
+        public void SetVolume(int volume)
+        {
+            Console.WriteLine($"Setting volume to {volume}");
+            Volume = volume;
+        }
+    }
+
+    // Facade: Home Theater
+    public class HomeTheaterFacade
+    {
+        public DVDPlayer dvdPlayer;
+        public Projector projector;
+        public AudioSystem audioSystem;
+
+        public HomeTheaterFacade()
+        {
+            dvdPlayer = new DVDPlayer();
+            projector = new Projector();
+            audioSystem = new AudioSystem();
+        }
+
+        public void WatchMovie(string movie)
+        {
+            Console.WriteLine("Get ready to watch a movie...");
+            dvdPlayer.TurnOn();
+            projector.TurnOn();
+            projector.SetInput("DVD Player");
+            audioSystem.TurnOn();
+            audioSystem.SetVolume(10);
+            dvdPlayer.PlayMovie(movie);
+        }
+
+        public void EndMovie()
+        {
+            Console.WriteLine("Shutting down the home theater...");
+            dvdPlayer.TurnOff();
+            projector.TurnOff();
+            audioSystem.TurnOff();
+        }
+    }
+}
+
---
+++ DesignPatterns/DesignPatternsTests/04-FacadeTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using DesignPatterns;
+
+namespace DesignPatternsTests
+{
+    public class FacadeTests
+    {
+        [Test]
+        public void WatchMovie_ShouldTurnOnAllComponents()
+        {
+            // Arrange
+            HomeTheaterFacade theater = new HomeTheaterFacade();
+
+            // Act
+            theater.WatchMovie("Test Movie");
+
+            // Assert
+            Assert.IsTrue(theater.dvdPlayer.IsOn);
+            Assert.IsTrue(theater.projector.IsOn);
+            Assert.IsTrue(theater.audioSystem.IsOn);
+            Assert.That(theater.projector.InputName == "DVD Player");
+            Assert.That(theater.audioSystem.Volume == 10);
+            Assert.That(theater.dvdPlayer.MovieName == "Test Movie");
+        }
+
+        [Test]
+        public void EndMovie_ShouldTurnOffAllComponents()
+        {
+            // Arrange
+            HomeTheaterFacade theater = new HomeTheaterFacade();
+
+            // Act
+            theater.WatchMovie("Test Movie");
+            theater.EndMovie();
+
+            // Assert
+            Assert.IsFalse(theater.dvdPlayer.IsOn);
+            Assert.IsFalse(theater.projector.IsOn);
+            Assert.IsFalse(theater.audioSystem.IsOn);
+            Assert.That(theater.projector.InputName == "");
+            Assert.That(theater.audioSystem.Volume == 10);
+            Assert.That(theater.dvdPlayer.MovieName == "");
+        }
+
+        [Test]
+        public void WatchMovie_Twice_ShouldNotAffectPreviousState()
+        {
+            // Arrange
+            HomeTheaterFacade theater = new HomeTheaterFacade();
+
+            // Act
+            theater.WatchMovie("First Movie");
+            theater.WatchMovie("Second Movie");
+
+            // Assert
+            Assert.IsTrue(theater.dvdPlayer.IsOn);
+            Assert.IsTrue(theater.projector.IsOn);
+            Assert.IsTrue(theater.audioSystem.IsOn);
+            Assert.That(theater.dvdPlayer.MovieName == "Second Movie");
+        }
+    }
+}
+
